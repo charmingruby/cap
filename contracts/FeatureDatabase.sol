63e3ed11e9150c892071bd4fa58382ebc38ebf2a
charmingruby <mrrubycanal3105@gmail.com>
2025-03-26 13:39:39 -0300

    feat: creates feature database contract

---
+++ contracts/FeatureDatabase.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract FeatureDatabase {
+    struct Feature {
+        string title;
+        string description;
+        string status;
+        uint8 priority;
+        uint16 expires_at;
+    }
+
+    mapping(uint32 => Feature) features;
+
+    uint256 public amount = 0;
+    uint32 private nextId = 0;
+    address private immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function store(Feature memory feat) public {
+        nextId++;
+        features[nextId] = feat;
+        amount++;
+    }
+
+    function save(uint32 id, Feature memory feat) public {
+        Feature memory featureFound = features[id];
+
+        require(!compareStr(featureFound.title, ""), "Feature does not exist");
+
+        if (
+            !compareStr(featureFound.title, feat.title) &&
+            !compareStr(feat.title, "")
+        ) features[id].title = feat.title;
+
+        if (
+            !compareStr(featureFound.description, feat.description) &&
+            !compareStr(feat.description, "")
+        ) features[id].description = feat.description;
+
+        if (
+            !compareStr(featureFound.status, feat.status) &&
+            !compareStr(feat.status, "")
+        ) features[id].status = feat.status;
+
+        if (featureFound.priority != feat.priority)
+            features[id].priority = feat.priority;
+
+        if (featureFound.expires_at != feat.expires_at)
+            features[id].expires_at = feat.expires_at;
+    }
+
+    function remove(uint32 id) public restricted {
+        require(!compareStr(features[id].title, ""), "Feature does not exist");
+
+        delete features[id];
+        amount--;
+    }
+
+    modifier restricted() {
+        require(owner == msg.sender, "Only the owner can remove features");
+        _;
+    }
+
+    function compareStr(
+        string memory str1,
+        string memory str2
+    ) private pure returns (bool) {
+        bytes memory arrA = bytes(str1);
+        bytes memory arrB = bytes(str2);
+
+        return
+            (arrA.length == arrB.length) &&
+            (keccak256(abi.encodePacked(str1)) ==
+                keccak256(abi.encodePacked(str2)));
+    }
+}
--- ignition/modules/FeatureDatabase.ts
+++ ignition/modules/FeatureDatabase.ts
@@ -6,15 +6,15 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI: bigint = 1_000_000_000n;
 
-const LockModule = buildModule("LockModule", (m) => {
+const FeatureDatabase = buildModule("FeatureDatabase", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
+  const featureDb = m.contract("FeatureDatabase", [unlockTime], {
     value: lockedAmount,
   });
 
-  return { lock };
+  return { featureDb };
 });
 
-export default LockModule;
+export default FeatureDatabase;
