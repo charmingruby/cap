d8e45e74fdd5fdb941a893b97db9ffa13059c62d
charmingruby <mrrubycanal3105@gmail.com>
2025-03-27 16:04:35 -0300

    feat: hardhat local deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  defaultNetwork: "local",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
+  },
 };
 
 export default config;
--- ignition/modules/FeatureDatabase.ts
+++ ignition/modules/FeatureDatabase.ts
@@ -3,16 +3,8 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
-
 const FeatureDatabase = buildModule("FeatureDatabase", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
-
-  const featureDb = m.contract("FeatureDatabase", [unlockTime], {
-    value: lockedAmount,
-  });
+  const featureDb = m.contract("FeatureDatabase");
 
   return { featureDb };
 });
